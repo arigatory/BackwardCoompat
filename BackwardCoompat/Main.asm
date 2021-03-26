528aa31882a9e67bcd366c48350ed35872360588
Ivan MSI <arigatory@gmail.com>
2021-03-26 18:45:24 +0300

    16 bit

--- BackwardCoompat/Main.asm
+++ BackwardCoompat/Main.asm
@@ -1,4 +1,4 @@
-
+;----------------x86-64bit----------------------------
 ;.code
 ;main	proc
 ;		mov rax,2
@@ -8,14 +8,27 @@
 ;main	endp
 ;end
 
+;----------------x86-32bit-----------------------------
+;.386
+;.model flat
+;.code
+
+;main	proc
+;		mov eax,2
+;		mov ebx,3
+;		mov ecx,4
+;		ret
+;main	endp
+;end		main
+;---------------16bit------------------------------------
 .386
 .model flat
 .code
 
 main	proc
-		mov eax,2
-		mov ebx,3
-		mov ecx,4
+		mov ax,2
+		mov bx,3
+		mov cx,4
 		ret
 main	endp
 end		main
